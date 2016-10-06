c8ee01f583e13623a410864a60e3b7e8af454934
yowchun93 <yowchun93@gmail.com>
2016-10-06 09:46:48 +0800

    quick union correct implementation

--- week_1/quick_union.rb
+++ week_1/quick_union.rb
@@ -6,11 +6,15 @@ end
 
 @id = []
 
-(1..9).each do |i|
+(0..9).each do |i|
   @id[i] = i
 end
 
+# not sure why there is nil
+@id.delete(nil)
+
 # find site which has a link to itself
+# in other words, its finding the root of a node/site
 def find(a) 
   until @id[a] == a
     a = @id[a]
@@ -18,10 +22,27 @@ def find(a)
   return a
 end
 
-puts find(5)
-
 # union
 def union(p,q)
-
+  p_root = find(p)
+  q_root = find(q)
+  
+  @id[p_root] = q_root
 end
+
+union(4,3)
+puts find(4)
+union(3,8)
+puts find(3) # ==> 8
+puts find(4) # ==> 8
+
+union(6,5) 
+puts find(6) # ==> 5
+# union(0,1)
+# union(0,2)
+# union(0,7)
+# union(1,5)
+# union(5,6)
+
+# what should be the number?
 
